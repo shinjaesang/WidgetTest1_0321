d02dd73da89f5c7558043c608ef1b8552d5ae995
shinjaesang <tlswltkd1011@gmail.com>
2024-03-21 16:27:09 +0900

    visibility 속성

---
+++ app/src/main/java/kr/ac/widgettest1_0321/MainActivity3.kt
@@ -0,0 +1,11 @@
+package kr.ac.widgettest1_0321
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main3)
+    }
+}
